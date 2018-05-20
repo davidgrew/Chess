d167b0415e7e86fcdcb2c077016bec089e13ecde
David G <davidgrew@hotmail.co.uk>
2018-05-20 21:37:27 +0100

    Add bishop move validation. Refactor castle move validation

--- src/com/davidgrew/Chess/Bishop.java
+++ src/com/davidgrew/Chess/Bishop.java
@@ -22,4 +22,27 @@ public class Bishop extends ChessPiece {
             this.shortName = "WB";
         }
     }
+    
+    public Boolean isMoveValid(ChessBoard board, ChessBoardSquare currentSquare, ChessBoardSquare futureSquare, ChessPlayer currentPlayer) {
+    
+        if (!futureSquare.isSquareEmpty && futureSquare.currentPiece.colour.equals(currentPlayer.getPieceColour()))
+            return false;  
+        
+        int distanceMovedXAxis = currentSquare.xAxisLocation - futureSquare.xAxisLocation;
+        int distanceMovedYAxis = currentSquare.yAxisLocation - futureSquare.yAxisLocation;
+        int unsignedDistanceMovedXAxis = distanceMovedXAxis < 0 ? distanceMovedXAxis * -1 : distanceMovedXAxis;
+        int unsignedDistanceMovedYAxis = distanceMovedYAxis < 0 ? distanceMovedYAxis * -1 : distanceMovedYAxis;
+        
+        if (unsignedDistanceMovedXAxis == unsignedDistanceMovedYAxis) {
+            int yAxisCounter = distanceMovedYAxis < 0 ? currentSquare.yAxisLocation+1 : currentSquare.yAxisLocation-1;
+            int xAxisCounter = distanceMovedXAxis < 0 ? currentSquare.xAxisLocation+1 : currentSquare.xAxisLocation-1;;
+            for (int i = 0; i <unsignedDistanceMovedXAxis; i++) {
+                if (!board.board[yAxisCounter][xAxisCounter].isSquareEmpty)
+                    return false;
+            }
+            return true;
+        }
+        else 
+            return false;
+    }
 }
--- src/com/davidgrew/Chess/Castle.java
+++ src/com/davidgrew/Chess/Castle.java
@@ -32,52 +32,11 @@ public class Castle extends ChessPiece {
         
         int distanceMovedXAxis = currentSquare.xAxisLocation - futureSquare.xAxisLocation;
         int distanceMovedYAxis = currentSquare.yAxisLocation - futureSquare.yAxisLocation;
-        
-//        if ((distanceMovedXAxis != 0 && distanceMovedYAxis == 0) || (distanceMovedXAxis == 0 && distanceMovedYAxis != 0)) {
-//            String whichAxis;
-//            if (distanceMovedXAxis == 0)
-//                whichAxis = "getYAxisLocation";
-//            else
-//                whichAxis = "getXAxisLocation";
-//            try {
-//                Class noparams[] = {};
-//                Class cls = Class.forName("com.davidgrew.BoardGames.Chess.ChessBoardSquare");
-//                Method newMethod = cls.getMethod("getYAxisLocation", noparams);
-//                int currentSquareLocation = (int) newMethod.invoke(currentSquare, (Object) null);
-//                int futureSquareLocation = (int) newMethod.invoke(futureSquare, (Object) null);
-//                int counter = currentSquareLocation;
-//                while (counter != futureSquareLocation) {
-//                    if (distanceMovedXAxis > 0 && !board.board[currentSquare.getYAxisLocation()][counter].isSquareEmpty)
-//                        return false;
-//                    else if (!board.board[counter][currentSquare.getXAxisLocation()].isSquareEmpty)
-//                        return false;
-//                    else
-//                        if (currentSquareLocation > futureSquareLocation)
-//                            counter++;
-//                        else
-//                            counter--;
-//                }
-//                return true;
-//            } catch (Exception e) { 
-//                return false;
-//            }            
-//        }
-//        else
-//            return false;
-
-        
-        
+                
         if (distanceMovedXAxis != 0 && distanceMovedYAxis == 0) {
-            int xAxisStart;
-            int xAxisEnd;
-            if (distanceMovedXAxis > 0) {
-                xAxisStart = currentSquare.xAxisLocation+1;
-                xAxisEnd = futureSquare.xAxisLocation;
-            }
-            else {
-                xAxisStart = futureSquare.xAxisLocation+1;
-                xAxisEnd = currentSquare.xAxisLocation;
-            }
+            boolean positiveMovement = distanceMovedXAxis > 0;
+            int xAxisStart = positiveMovement ? currentSquare.xAxisLocation+1 : futureSquare.xAxisLocation+1;
+            int xAxisEnd = positiveMovement ? futureSquare.xAxisLocation : currentSquare.xAxisLocation;
             for (int i = xAxisStart; i < xAxisEnd; i++) {
                 if (!board.board[currentSquare.yAxisLocation][i].isSquareEmpty)
                     return false;
@@ -85,17 +44,9 @@ public class Castle extends ChessPiece {
             return true;
         }
         else if (distanceMovedXAxis == 0 && distanceMovedYAxis != 0) {
-            int yAxisStart;
-            int yAxisEnd;
-            if (distanceMovedYAxis < 0) {
-                yAxisStart = currentSquare.yAxisLocation+1;
-                yAxisEnd = futureSquare.yAxisLocation;
-            }
-            else {
-                String s = "yAxisLocation";
-                yAxisStart = futureSquare.yAxisLocation+1;
-                yAxisEnd = currentSquare.yAxisLocation;
-            }
+            boolean positiveMovement = distanceMovedYAxis < 0;
+            int yAxisStart = positiveMovement ? currentSquare.yAxisLocation+1 : futureSquare.yAxisLocation+1;
+            int yAxisEnd = positiveMovement ? futureSquare.yAxisLocation : currentSquare.yAxisLocation;
             for (int i = yAxisStart; i < yAxisEnd; i++) {
                 if (!board.board[i][currentSquare.xAxisLocation].isSquareEmpty)
                     return false;
