8b44ddaaa0c7f76690ce1cf31932d46d0edaf38f
David G <davidgrew@hotmail.co.uk>
2018-05-20 21:48:55 +0100

    Add Queen move validation

--- src/com/davidgrew/Chess/Queen.java
+++ src/com/davidgrew/Chess/Queen.java
@@ -22,4 +22,47 @@ public class Queen extends ChessPiece {
             this.shortName = "WQ";
         }
     }
+    
+    public Boolean isMoveValid(ChessBoard board, ChessBoardSquare currentSquare, ChessBoardSquare futureSquare, ChessPlayer currentPlayer) {
+
+        if (!futureSquare.isSquareEmpty && futureSquare.currentPiece.colour.equals(currentPlayer.getPieceColour()))
+            return false; 
+    
+        int distanceMovedXAxis = currentSquare.xAxisLocation - futureSquare.xAxisLocation;
+        int distanceMovedYAxis = currentSquare.yAxisLocation - futureSquare.yAxisLocation;
+        int unsignedDistanceMovedXAxis = distanceMovedXAxis < 0 ? distanceMovedXAxis * -1 : distanceMovedXAxis;
+        int unsignedDistanceMovedYAxis = distanceMovedYAxis < 0 ? distanceMovedYAxis * -1 : distanceMovedYAxis;
+        
+        if (unsignedDistanceMovedXAxis == unsignedDistanceMovedYAxis) {
+            int yAxisCounter = distanceMovedYAxis < 0 ? currentSquare.yAxisLocation+1 : currentSquare.yAxisLocation-1;
+            int xAxisCounter = distanceMovedXAxis < 0 ? currentSquare.xAxisLocation+1 : currentSquare.xAxisLocation-1;
+            for (int i = 1; i < unsignedDistanceMovedXAxis; i++) {
+                if (!board.board[yAxisCounter][xAxisCounter].isSquareEmpty)
+                    return false;
+            }
+            return true;
+        }
+        else if (distanceMovedXAxis != 0 && distanceMovedYAxis == 0) {
+            boolean positiveMovement = distanceMovedXAxis > 0;
+            int xAxisStart = positiveMovement ? currentSquare.xAxisLocation+1 : futureSquare.xAxisLocation+1;
+            int xAxisEnd = positiveMovement ? futureSquare.xAxisLocation : currentSquare.xAxisLocation;
+            for (int i = xAxisStart; i < xAxisEnd; i++) {
+                if (!board.board[currentSquare.yAxisLocation][i].isSquareEmpty)
+                    return false;
+            }
+            return true;
+        }
+        else if (distanceMovedXAxis == 0 && distanceMovedYAxis != 0) {
+            boolean positiveMovement = distanceMovedYAxis < 0;
+            int yAxisStart = positiveMovement ? currentSquare.yAxisLocation+1 : futureSquare.yAxisLocation+1;
+            int yAxisEnd = positiveMovement ? futureSquare.yAxisLocation : currentSquare.yAxisLocation;
+            for (int i = yAxisStart; i < yAxisEnd; i++) {
+                if (!board.board[i][currentSquare.xAxisLocation].isSquareEmpty)
+                    return false;
+            }
+            return true;
+        }
+        else
+            return false;
+    }
 }
